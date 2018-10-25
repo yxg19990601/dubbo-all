3973db72c971bb301f74d8e58b00e510a223bc98
ideayxg <175@qq.com>
2018-10-25 21:24:24 +0800

    boot 整合 dubbo

---
+++ boot-user-service-provider/src/main/java/com/xucm/dubbo/user/BootUserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.xucm.dubbo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BootUserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUserServiceProviderApplication.class, args);
+    }
+}
